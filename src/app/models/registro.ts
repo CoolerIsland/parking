14a37df8cadf67779342ff38fca67b599d760595
CoolerIsland <eddisson92@gmail.com>
2023-05-15 18:30:50 -0500

    Guardar registro

---
+++ src/app/models/registro.ts
@@ -0,0 +1,8 @@
+export class Registro {
+  IdVehiculo: number = 0;
+  placa: string = '';
+  tipo: string = '';
+  aplicaDescuento: boolean = false;
+  horaIngreso: string = '';
+  horaSalida: string = '';
+}
